2f3d0df1d0b4b5677d9d0b9be0b36ed889c81c41
galitsky.e.o <galitsky.e.o@gmail.com>
2017-07-31 22:41:19 +0300

    add transition

--- src/js/main.js
+++ src/js/main.js
@@ -1,150 +1,121 @@
+'use strict';
 function ScrollRevealAnimation(wrapper, config) {
     this.wrapper = window[wrapper] || document.getElementsByClassName(wrapper);
     this.wrapperChildren = this.wrapper.children;
+    this.body = document.getElementsByTagName('body');
+    this.transformHeight = 0;
     this.config = {
-        scrollDuration: 100
+        scrollDuration: 200,
+        animation: 'ease'
     };
-    this.animationFlag = true;
 
     // Initialize function
     this.init = function () {
+        this.disableBodyScroll();
+        this.buildConfig(config);
         this.initSections();
         this.resize();
         this.scrollSectionAnimation();
         this.detectActiveSection();
     };
-    
-    // Calculate sections height
-    this.sectionsHeight = function(){
-        this.windowHeight = document.documentElement.clientHeight;
-        this.sectionsDocumentPosition = [];
-        var counter = 0;
-        for (var item in this.wrapperChildren) {
-            if(!isNaN(item)) {
-                var itemElem = this.wrapperChildren[item];
-                itemElem.style.cssText = 'height:'+this.windowHeight+'px;';
-                this.calculateSectionsDocumentPosition(this.windowHeight*counter);
-                counter++;
-            }
+
+    // Build config
+    this.buildConfig = function(config){
+        for (var option in config) {
+            this.config[option] = config[option];
         }
-        this.currentScroll = window.pageYOffset;
-        this.documentHeight = this.wrapper.offsetHeight;
+    };
+
+    // Body scroll disable
+    this.disableBodyScroll = function () {
+        this.body[0].style.cssText = 'overflow:hidden;';
+        this.wrapper.style.cssText = '-webkit-transition-timing-function:'+this.config.animation+';'+
+                                  '-moz-transition-timing-function:'+this.config.animation+';'+
+                                  '-o-transition-timing-function:'+this.config.animation+';'+
+                                  'transition-timing-function:'+this.config.animation+';'+
+                                  '-webkit-transition-duration:'+this.config.scrollDuration/1000+'s;'+
+                                  '-moz-transition-duration:'+this.config.scrollDuration/1000+'s;'+
+                                  '-o-transition-duration:'+this.config.scrollDuration/1000+'s;'+
+                                  'transition-duration:'+this.config.scrollDuration/1000+'s;';
     };
 
     // Set section background image, height and section number on init
     this.initSections = function(){
-        for (var item in this.wrapperChildren) {
-            var itemElem = this.wrapperChildren[item];
-            if(!isNaN(item)) {
-                var itemImageData = itemElem.getAttribute('data-img');
-                this.setBackground(itemImageData, itemElem);
-                itemElem.removeAttribute('data-img');
-            }
+        for (var i = 0; i<this.wrapperChildren.length;i++) {
+            var itemElem = this.wrapperChildren[i];
+            var itemImageData = itemElem.getAttribute('data-img');
+            itemElem.removeAttribute('data-img');
+            this.setBackground(itemImageData, itemElem);
         }
+
         this.sectionsHeight();
     };
 
     this.setBackground = function(img, section){
         if(img){
             var overlay = document.createElement('div');
-            overlay.classList.add('overlay');
-            overlay.style.cssText = 'position:absolute;left:0;top:0;z-index:0;width:100%;height:100%;background-size:cover;background-position:center top;background-repeat:no-repeat;';
-            overlay.style.backgroundImage = 'url('+img+')';
+            overlay.className += 'reveal-overlay';
+            overlay.style.cssText = 'position:absolute;'+
+              'left:0;'+
+              'top:0;'+
+              'z-index:0;'+
+              'width:100%;'+
+              'height:100%;'+
+              'background-size:cover;'+
+              'background-position:center top;'+
+              'background-repeat:no-repeat;'+
+              'background-image:url('+img+');';
             section.insertBefore(overlay,section.children[0]);
         }
     };
 
-    // Create array with position of every section 
-    this.calculateSectionsDocumentPosition = function(position){
-        this.sectionsDocumentPosition.push(position);
+    // Calculate sections height
+    this.sectionsHeight = function(){
+        this.windowHeight = document.documentElement.clientHeight;
+        for (var i = 0; i < this.wrapperChildren.length; i++) {
+            var itemElem = this.wrapperChildren[i];
+            itemElem.style.cssText = 'height:'+this.windowHeight+'px;';
+        }
     };
 
     // Detect user scroll
     this.scrollSectionAnimation = function () {
         var self = this;
-
+        self.animationFlag = true;
         window.onwheel = function (event) {
-            var topWindowScroll = window.pageYOffset;
-            var stopWheelEnd = self.documentHeight-self.windowHeight;
-            if(!self.animationFlag) {
-                return;
-            }
-            if(event.deltaY>0 && topWindowScroll != stopWheelEnd) {
-                self.direction = true;
-                self.scrollSection(topWindowScroll, topWindowScroll + self.windowHeight,event.deltaY);
-            } else if(event.deltaY<0 && topWindowScroll != 0) {
-                self.direction = false;
-                self.scrollSection(topWindowScroll, topWindowScroll - self.windowHeight,event.deltaY);
+            event.preventDefault();
+            self.scrollDirection =  event.deltaY > 0;
+            if(self.animationFlag && self.scrollDirection) {
+                self.transformHeight = self.transformHeight + self.windowHeight;
+                self.scrollSection();
+            } else if(self.animationFlag) {
+
             }
         }
     };
 
-    // Scroll section
-    this.scrollSection = function(top, destination){
-        this.animation(top, destination);
-    };
-
-    // Animation function
-    this.animation = function(start, to){
-        var self = this;
-        var beginAnim = new Date().getTime();
-        self.animationFlag = false;
-        setTimeout(function() {
-            var now = (new Date().getTime()) - beginAnim;
-            var progress = now / 700;
-            var result = (to - start) * self.delta(progress) + start;
-            if(result>to && self.direction || result<to && !self.direction) {
-                result = to;
-            }
-            window.scrollTo(start,result);
-            self.checkProgress(progress,to,arguments.callee);
-        }, 5);
-    };
-
-    this.delta = function(progress) {
-        return progress;
-    };
-
-    this.checkProgress = function(progress,to,time){
-        var self = this;
-        if (progress < 1) {
-            setTimeout(time, 5)
-        } else {
-            self.currentScroll = window.pageYOffset;
-            self.detectActiveSection();
-            self.animationFlag = true;
-            this.parallaxAnimation();
-        };
+    // Sections movement
+    this.scrollSection = function () {
+        var move = '-webkit-transform: translate('+this.transformHeight+'px,0);'+
+                    '-moz-transform: translate('+this.transformHeight+'px,0);'+
+                    '-ms-transform: translate('+this.transformHeight+'px,0);'+
+                    '-o-transform: translate('+this.transformHeight+'px,0);'+
+                    'transform: translate('+this.transformHeight+'px,0);';
+        this.wrapper.style.cssText = move;
     };
 
     // Parallax animation
     this.parallaxAnimation = function(){
-        var self = this;
-        var parallax = self.windowHeight*70/100;
-        if(self.currentSlide != self.wrapperChildren.length){
-            var paralElem = self.wrapperChildren[self.currentSlide+1].children[0];
-            paralElem.style.transform = 'translateY(-'+parallax+'px)';
-        }
+        var parallax = this.windowHeight*70/100;
     };
 
     // Detect active section
     this.detectActiveSection = function(){
-        var self = this;
-        self.sectionsDocumentPosition.forEach(function(item,num){
-            var elem = self.wrapperChildren[num];
-            var nextElem = self.wrapperChildren[num+1];
-            if(item == self.currentScroll) {
-                self.currentSlide = num;
-                elem.classList.add('current-slide');
-            } else  {
-                elem.classList.remove('current-slide');
-            }
-        })
+
     };
 
     // Show one slide on resize
-    this.showOneSLideOnResize = function(){
-        var self = this;
+    this.showOneSlideOnResize = function(){
         window.scrollTo(0, self.currentSlide * self.windowHeight);
     };
 
@@ -153,7 +124,7 @@ function ScrollRevealAnimation(wrapper, config) {
         var self = this;
         window.onresize = function () {
             self.sectionsHeight();
-            this.showOneSLideOnResize();
+            self.showOneSlideOnResize();
         };
     };
 
